689368b2d62031e203dc2b20d10403633c4d61a6
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-19 00:18:03 +0200

    adds class Api

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import '../node_modules/focus-visible/dist/focus-visible';
-import "../src/scss/main.scss";
-import "../src/index.html";
-
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,157 +1,83 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-const { extendDefaultPlugins } = require("svgo");
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+module.exports = {
+  entry: {
+    polyfill: 'babel-polyfill',
+    app: './js/index.js',
+  },
+  context: path.resolve(__dirname, 'src'),
+  devServer: {
+    publicPath: '/',
+    port: 1989,
+    contentBase: path.join(process.cwd(), 'dist'),
+    host: 'localhost',
+    historyApiFallback: true,
+    noInfo: false,
+    stats: 'minimal',
+    hot: true,
+  },
+  module: {
 
-const isDev = process.env.NODE_ENV === "development";
-const isProd = !isDev;
+    rules: [
+      {
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+        test: /\.js$/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
 
-const optimization = () => {
-    const configObj = {
-     splitChunks: {
-         chunks: 'all'
-     }
-    };
-    if (isProd) {
-        configObj.minimizer = [
-         new OptimizeCssAssetsWebpackPlugin (),
-         new TerserWebpackPlugin()
-        ];
-    }
-    return configObj;
-};
-
-const plugins = () => {
-    const basePlugins = [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'src/index.html'),
-            filename: 'index.html',
-            minify: {
-                collapseWhitespace: isProd,
+            options: {
+              importLoaders: 1,
+              sourceMap: true,
             },
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: `./css/${filename('css')}`
-        })
-    ];
-     if(isProd){
-         basePlugins.push(
-    new ImageMinimizerPlugin({
-      minimizerOptions: {
-        // Lossless optimization with custom option
-        // Feel free to experiment with options for better result for you
-        plugins: [
-          ["gifsicle", { interlaced: true }],
-          ["jpegtran", { progressive: true }],
-          ["optipng", { optimizationLevel: 5 }],
-          // Svgo configuration here https://github.com/svg/svgo#configuration
-          [
-            "svgo",
-            {
-              plugins: extendDefaultPlugins([
-                {
-                  name: "removeViewBox",
-                  active: false,
-                },
-                {
-                  name: "addAttributesToSVGElement",
-                  params: {
-                    attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
-                  },
-                },
-              ]),
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [precss, autoprefixer],
             },
-          ],
+          },
         ],
       },
-    }),
-         )
-     }
-    return basePlugins;
-}
-
-const filename = (ext) => isDev
-    ? `[name].${ext}`
-    : `[name].[contenthash].${ext}`;
-
- module.exports = {
-    context: path.resolve(__dirname, 'src'),
-     mode: 'development',
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: `./js/${filename('js')}`,
-        publicPath: ''
-    },
-    devServer:{
-        historyApiFallback:{
-            index:'build/index.html'
-        },
-        static: path.resolve(__dirname, 'build'),
-        open: true,
-         client: {
-      logging: 'error'
-    },
-        compress: true,
-        port: 4444,
-    },
-    optimization: optimization(),
-     plugins: plugins(),
-     devtool: isProd ? false : "source-map",
-     module:{
-         rules: [
-             {
-                 test: /\.html$/,
-                use:["html-loader"]
-             },
-             {
-                test: /\.js$/,
-                exclude: /node_modules/, 
-                use: ['babel-loader'],
-              },
-            {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader, "css-loader"
-                ],
-              },
-              {
-                    test: /\.s[ac]ss$/,
-                    use: [{
-                        loader: MiniCssExtractPlugin.loader,
-                        options:{
-                            publicPath: (resourcePath, context) => {
-                                return path.relative(path.dirname(resourcePath), context) + '/';
-                            },
-                        }
-                    },
-                "css-loader", 
-                "sass-loader"
-                ],
-                },
-                {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        type: "asset",
-      },
-        
-            {
-                test: /\.(?:|woff2)$/,
-                use: [{
-                    loader:'file-loader',
-                    options: {
-                    name: `./fonts/${filename('[ext]')}`     
-                    },
-                }],
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
             },
-         ]
-     }
-  };
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+  },
+  mode: 'development',
+};
 
 
 
